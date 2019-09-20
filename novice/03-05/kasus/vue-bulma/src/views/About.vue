206778c9670b003d2abe9baa7145afb6feea4cdc
hijrahsaputra@gmail.com <hijrahsaputra@gmail.com>
2019-09-20 19:41:09 +0700

    add novice 03-05 vue js

---
+++ novice/03-05/kasus/vue-bulma/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1 class="title">About</h1>
+    <p class="is-medium">This is the Todolist app v.1.0. It is part of the Vue crash course </p>
+  </div>
+</template>
