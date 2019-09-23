9032accefce04c398b1277f9eb20f669a8284ccd
hijrahsaputra@gmail.com <hijrahsaputra@gmail.com>
2019-09-23 22:05:50 +0700

    add novice 04-01 vue js

---
+++ novice/04-01/latihan/css-transitions/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+   <Transitions/>
+  </div>
+</template>
+
+<script>
+import Transitions from './components/Transitions'
+export default {
+  name: 'app',
+  components: {
+    Transitions
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ novice/04-01/latihan/css-transitions/src/components/Transitions.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="example-1">
+        <button @click="show = !show">
+            Toggle render
+        </button>
+        <transition name="slide-fade">
+            <p v-if="show">hello</p>
+        </transition>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Transitions'
+}
+</script>
+<style scoped>
+    /* Enter and leave animations can use different */
+    /* durations and timing functions.              */
+    .slide-fade-enter-active {
+    transition: all .3s ease;
+    }
+    .slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active below version 2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+    }
+</style>
