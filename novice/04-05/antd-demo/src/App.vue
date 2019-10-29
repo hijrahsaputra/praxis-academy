76149650b9031db6e2877e539919290f35f365f8
hijrahsaputra@gmail.com <hijrahsaputra@gmail.com>
2019-10-29 22:49:57 +0700

    add 04-05 testing devtools

---
+++ novice/04-05/antd-demo/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <TopMenu />
+    <Breadcrumb />
+    <Form />
+  </div>
+</template>
+<script>
+import Form from './components/Form.vue'
+import TopMenu from './components/TopMenu'
+import Breadcrumb from './components/Breadcrumb'
+
+export default {
+  name: 'app',
+  components: {
+    Form,
+    TopMenu,
+    Breadcrumb
+  }
+}
+</script>
