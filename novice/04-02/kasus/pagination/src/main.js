42dcd81020bda638452bc369c88c31a103c5349a
hijrahsaputra@gmail.com <hijrahsaputra@gmail.com>
2019-10-29 23:11:29 +0700

    add kasus 04-02 pagination

---
+++ novice/04-02/kasus/pagination/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import JwPagination from 'jw-vue-pagination';
+Vue.component('jw-pagination', JwPagination);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
